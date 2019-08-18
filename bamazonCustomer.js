49120952bba08a2959fcf39bd92b0887339ef2c6
Sathish Pittala <48255299+sathishbaburaj@users.noreply.github.com>
2019-08-18 19:06:22 -0400

    added customer application java script to place the orders

---
+++ bamazonCustomer.js
@@ -0,0 +1,111 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table2");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "bamazon_db",
+  port: 3306
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+});
+
+var display = function() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log("------------------------------------------------------------");
+    console.log("Welcome to Bamazon");
+    console.log("------------------------------------------------------------");
+    console.log("");
+    console.log("Find your product below");
+
+    var table = new Table({
+      head: ["Product Id", "Product Descrioption", "Cost"],
+      colWidths: [12, 50, 8],
+      colAligns: ["center", "left", "right"],
+      style: {
+        head: ["aqua"],
+        compact: true
+      }
+    });
+    for (var i = 0; i < res.length; i++) {
+      table.push([res[i].id, res[i].products_name, res[i].price]);
+    }
+
+    console.log(table.toString());
+    console.log("");
+    shopping();
+
+    // connection.end();
+    // instantiate
+  });
+};
+
+var shopping = function() {
+  inquirer
+    .prompt({
+      name: "producttoBuy",
+      type: "input",
+      message:
+        "Please enter the product id of the item that you want to purchase"
+    })
+    .then(function(answer) {
+      var selection = answer.producttoBuy;
+      connection.query("SELECT * FROM products WHERE id=?", selection, function(
+        err,
+        res
+      ) {
+        if (err) throw err;
+        if (res.length === 0) {
+          console.log(
+            "That product doesn't exist,Please enter a Product from the list above"
+          );
+          shopping();
+        } else {
+          inquirer
+            .prompt({
+              name: "quantity",
+              type: "iput",
+              message: "How many items would you like to purchase?"
+            })
+            .then(function(answer1) {
+              var quantity = answer1.quantity;
+              if (quantity > res[0].stock_quantity) {
+                console.log(
+                  "Sorry we have" +
+                    res[0].stock_quantity +
+                    "tems of the selected quantity"
+                );
+                shopping();
+              } else {
+                console.log("");
+                console.log(res[0].products_name + "purchased");
+                console.log(quantity + "qty@ $" + res[0].price);
+
+                var newQuantity = res[0].stock_quantity - quantity;
+                connection.query(
+                  "UPDATE products SET stock_quantity =" +
+                    newQuantity +
+                    "WHERE id =" +
+                    res[0].id,
+                  function(err, resUpdate) {
+                    if (err) throw err;
+                    console.log("");
+                    console.log("Your Order has been Processed");
+                    console.log("Thank you for Shopping with us...!");
+                    console.log("");
+                    connection.end();
+                  }
+                );
+              }
+            });
+        }
+      });
+    });
+};
+
+display();
